a46f7f0463c4a21064609530875b6a0a29ef5dfc
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-10-01 13:53:21 +0000

    Initial creation of project.  Activity Stubs.

---
+++ trunk/Voice_2_Social/src/com/v2s/mainMenu.java
@@ -0,0 +1,14 @@
+package com.v2s;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class mainMenu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
