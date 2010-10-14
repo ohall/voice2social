9a1116eb0dedf2c107fb013dfe19f87455b82b79
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-10-14 23:40:49 +0000

    Added some intent handling to mainMenu and some intent filters to manifest. Still working on connecting menu to peripheral activities. Compiles properly, but not yet functional.

--- trunk/Voice_2_Social/src/com/v2s/mainMenu.java
+++ trunk/Voice_2_Social/src/com/v2s/mainMenu.java
@@ -1,7 +1,12 @@
 package com.v2s;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 
 public class mainMenu extends Activity {
@@ -10,5 +15,26 @@ public class mainMenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        
+        Button reviewAndSendButton = 
+        	(Button) findViewById(R.id.reviewAndSendButton);
+        
+        
+
+	    reviewAndSendButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		//start review and send activity
+	    		startReviewAndSendActivity();
+	    	}
+	    });
+
+
+        }
+    
+
+	private void startReviewAndSendActivity() {
+	Intent reviewAndSendIntent = new Intent(this , reviewAndSend.class);
+	startActivity(reviewAndSendIntent);
+	}
+
 }
