83ec80e2083444102dba7bf7ca6382180736eb12
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-10-16 17:54:03 +0000

    Linked all activities to main menu via buttons which trigger intents from main. Created layouts for each activity.

--- trunk/Voice_2_Social/src/com/v2s/mainMenu.java
+++ trunk/Voice_2_Social/src/com/v2s/mainMenu.java
@@ -14,27 +14,96 @@ public class mainMenu extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+        
+
+        Button mediaSelectButton = 
+        	(Button) findViewById(R.id.mediaSelectButton);
+        
+        Button newUserLoginButton = 
+        	(Button) findViewById(R.id.newUserLoginButton);
+        
+        Button onAirButton = 
+        	(Button) findViewById(R.id.onAirButton);
+        
+        Button viewFriends = 
+        	(Button) findViewById(R.id.viewFriends);
         
         Button reviewAndSendButton = 
         	(Button) findViewById(R.id.reviewAndSendButton);
         
         
-
-	    reviewAndSendButton.setOnClickListener(new OnClickListener() {
+        	
+        	
+        mediaSelectButton.setOnClickListener(new OnClickListener() {
 	    	public void onClick(View view){
 	    		//start review and send activity
-	    		startReviewAndSendActivity();
+	    		launchActivity(1);
 	    	}
 	    });
+    	
+        onAirButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		//start review and send activity
+	    		launchActivity(2);
+	    	}
+	    });
+        
+        newUserLoginButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		//start review and send activity
+	    		launchActivity(3);
+	    	}
+	    });
+        
+        reviewAndSendButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		//start review and send activity
+	    		launchActivity(4);
+	    	}
+	    });
+        
+        viewFriends.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		//start review and send activity
+	    		launchActivity(5);
+	    	}
+	    });
+	    
+    	
 
 
         }
     
 
-	private void startReviewAndSendActivity() {
-	Intent reviewAndSendIntent = new Intent(this , reviewAndSend.class);
-	startActivity(reviewAndSendIntent);
+	private void launchActivity(int activity) {
+	
+		Intent reviewAndSendIntent = null;
+	
+		switch(activity){
+		
+			case 1:
+				reviewAndSendIntent = new Intent(this , mediaSelect.class);
+				break;
+			case 2:
+				reviewAndSendIntent = new Intent(this , onAir.class);
+				break;
+			case 3:
+				reviewAndSendIntent = new Intent(this , newUserLogin.class);
+				break;
+			case 4:
+				reviewAndSendIntent = new Intent(this , reviewAndSend.class);
+				break;
+			case 5:
+				reviewAndSendIntent = new Intent(this , viewFriends.class);
+				break;
+				
+		}
+		
+		startActivity(reviewAndSendIntent);
 	}
 
-}
+	
+	
+}//end activity
