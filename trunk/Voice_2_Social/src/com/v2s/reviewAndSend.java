aeaa35cfa0912b6ed655a961a0ffe47bcc7070cd
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-10-16 22:23:34 +0000

    extracted recorded data out of bundle for display in text view.

--- trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
+++ trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
@@ -2,6 +2,7 @@ package com.v2s;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class reviewAndSend extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,17 @@ public class reviewAndSend extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviewandsend);
+        
+        
+        //get bundle from intent and assign it to a string;
+        Bundle incomingBundle = this.getIntent().getExtras();
+        String text = incomingBundle.getString("DEFAULTTEXT");
+        
+       
+        TextView textForReview = (TextView) findViewById(R.id.textForReview);
+
+        //assign string to our textview
+        textForReview.setText(text);
+        
     }
 }
