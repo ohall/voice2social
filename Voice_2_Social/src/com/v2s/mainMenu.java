6d58a15f6bffb2b1f1c08a0e6d5e92cee505eeb8
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-11-06 23:42:26 +0000

    Implemented basic text to voice instruction and voice recognition command handling.

--- Voice_2_Social/src/com/v2s/mainMenu.java
+++ Voice_2_Social/src/com/v2s/mainMenu.java
@@ -1,31 +1,41 @@
 package com.v2s;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 
 
-public class mainMenu extends Activity {
-    /** Called when the activity is first created. */
+
+public class mainMenu extends Activity implements TextToSpeech.OnInitListener {
+    
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+	private static final String MAIN_MENU_INSTRUCTIONS = "Please say: Media, New User or Friends.";
+    
+    private TextToSpeech mTts; 
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.main);
-        
-
+        /*
+         * Create Buttons
+         */
         Button mediaSelectButton = 
         	(Button) findViewById(R.id.mediaSelectButton);
         
         Button newUserLoginButton = 
         	(Button) findViewById(R.id.newUserLoginButton);
-        
-//        Button onAirButton = 
-//        	(Button) findViewById(R.id.onAirButton);
         
         Button viewFriends = 
         	(Button) findViewById(R.id.viewFriends);
@@ -33,23 +43,16 @@ public class mainMenu extends Activity {
         Button reviewAndSendButton = 
         	(Button) findViewById(R.id.reviewAndSendButton);
         
-        
-        	
-        	
+        /*
+         * Set Button Click Listeners
+         */
         mediaSelectButton.setOnClickListener(new OnClickListener() {
 	    	public void onClick(View view){
 	    		//start review and send activity
 	    		launchActivity(1);
 	    	}
 	    });
-    	
-//        onAirButton.setOnClickListener(new OnClickListener() {
-//	    	public void onClick(View view){
-//	    		//start review and send activity
-//	    		launchActivity(2);
-//	    	}
-//	    });
-        
+   
         newUserLoginButton.setOnClickListener(new OnClickListener() {
 	    	public void onClick(View view){
 	    		//start review and send activity
@@ -70,7 +73,12 @@ public class mainMenu extends Activity {
 	    		launchActivity(5);
 	    	}
 	    });
-	    
+        
+        /**
+         * Text to Speech for Instructions
+         */
+        mTts = new TextToSpeech(this,this); //init text to speech
+
     }//end onCreate
     
 
@@ -79,12 +87,8 @@ public class mainMenu extends Activity {
 		Intent intent = null;
 	
 		switch(activity){
-		
 			case 1:
 				intent = new Intent(this , mediaSelect.class);
-				break;
-			case 2:
-				intent = new Intent(this , onAir.class);
 				break;
 			case 3:
 				intent = new Intent(this , newUserLogin.class);
@@ -99,12 +103,97 @@ public class mainMenu extends Activity {
 				intent = new Intent(this , viewFriends.class);
 				break;
 				
+				
+				
 		}//end switch
 		
 		startActivity(intent);
-	
+		
 	}//end launchActivity
-
 	
+	
+	
+	private void startVoiceRecognitionActivity() {
+		
+    	Intent commandIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        commandIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        //commandIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
+        startActivityForResult(commandIntent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+	
+	
+	
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        
+    	int selection = 0;
+
+    	if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+        	
+        	ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        	String selectCommand = ""; 
+
+        	
+        	selectCommand = matches.get(0);
+        	
+        	if( selectCommand.compareToIgnoreCase("media") == 0 ){
+        		selection = 1;
+        	}else if(selectCommand.compareToIgnoreCase("new user") == 0){
+        		selection = 3;
+        	}else if(selectCommand.compareToIgnoreCase("friends") == 0){
+        		selection = 5;
+        	}
+
+        	 super.onActivityResult(requestCode, resultCode, data);
+             
+        	 if(selection != 0){
+        		 launchActivity(selection);
+        	 }else{
+        		 startVoiceRecognitionActivity();
+        	 }
+        }
+
+       
+    }
+    
+    
+    private void sayit(String x) {
+		mTts.speak(x, TextToSpeech.QUEUE_FLUSH, null);
+	}
+    
+    
+
+
+	@Override
+	public void onInit(int status) {
+		
+		if (status == TextToSpeech.SUCCESS) {
+			
+			int result = mTts.setLanguage(Locale.getDefault());
+			
+			if (result == TextToSpeech.LANG_MISSING_DATA
+					|| result == TextToSpeech.LANG_NOT_SUPPORTED) {
+				mTts.setLanguage(Locale.US);
+			} 
+			
+			sayit(MAIN_MENU_INSTRUCTIONS);
+			
+			startVoiceRecognitionActivity();
+		}
+		
+	}
+	
+	@Override
+	public void onDestroy() {
+		// kill it!
+		if (mTts != null) {
+			mTts.stop();
+			mTts.shutdown();
+		}
+
+		super.onDestroy();
+	}
+
 	
 }//end activity
