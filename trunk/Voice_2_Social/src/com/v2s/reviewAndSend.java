77d81fb3d2477679b658172583878cb9a4d056c3
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-11-21 02:55:40 +0000

    beginning to implements button behaviors.

--- trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
+++ trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
@@ -1,7 +1,11 @@
 package com.v2s;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class reviewAndSend extends Activity {
@@ -10,6 +14,24 @@ public class reviewAndSend extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviewandsend);
+
+        Button reRecordButton = 
+        	(Button) findViewById(R.id.reRecordButton);
+        
+        Button acceptButton = 
+        	(Button) findViewById(R.id.acceptButton);
+        
+        reRecordButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		lauchActivity(1);
+	    	}
+	    });
+        
+        acceptButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		lauchActivity(2);
+	    	}
+	    });
         
         
         //get bundle from intent and assign it to a string;
@@ -22,5 +44,32 @@ public class reviewAndSend extends Activity {
         //assign string to our textview
         textForReview.setText(text);
         
+    }
+    
+    private void lauchActivity(int activity){
+    	
+    	Intent intent = null;
+		Bundle bundle = new Bundle();
+	
+		switch(activity){
+		
+			case 1:
+				//bundle.putString("DEFAULTTEXT", "Twitter");
+				intent = new Intent(this , mainMenu.class);
+				break;
+			case 2:
+				//bundle.putString("DEFAULTTEXT", "Facebook");
+				intent = new Intent(this , onAir.class);
+				break;
+//			case 3:
+//				bundle.putString("DEFAULTTEXT", "Google Buzz");
+//				intent = new Intent(this , onAir.class);
+//				break;
+				
+		}//end switch
+		
+		//intent.putExtras(bundle);
+		startActivity(intent);
+    	
     }
 }
