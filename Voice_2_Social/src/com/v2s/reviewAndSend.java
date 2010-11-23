59fe01437b7d977f4cd939632f4e7846391ccc50
joelmorales22@gmail.com <joelmorales22@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-11-23 23:38:09 +0000

    Implemented text to speech for the this activity

--- Voice_2_Social/src/com/v2s/reviewAndSend.java
+++ Voice_2_Social/src/com/v2s/reviewAndSend.java
@@ -1,75 +1,85 @@
 package com.v2s;
 
+import java.util.Locale;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
-public class reviewAndSend extends Activity {
+public class reviewAndSend extends Activity implements TextToSpeech.OnInitListener {
+	
+	private EditText textForReview;
+    private Button speakButton;
+    private TextToSpeech mTts; 
+    
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviewandsend);
-
-        Button reRecordButton = 
-        	(Button) findViewById(R.id.reRecordButton);
-        
-        Button acceptButton = 
-        	(Button) findViewById(R.id.acceptButton);
-        
-        reRecordButton.setOnClickListener(new OnClickListener() {
-	    	public void onClick(View view){
-	    		lauchActivity(1);
-	    	}
-	    });
-        
-        acceptButton.setOnClickListener(new OnClickListener() {
-	    	public void onClick(View view){
-	    		lauchActivity(2);
-	    	}
-	    });
-        
-        
         //get bundle from intent and assign it to a string;
         Bundle incomingBundle = this.getIntent().getExtras();
         String text = incomingBundle.getString("DEFAULTTEXT");
         
-       
-        TextView textForReview = (TextView) findViewById(R.id.textForReview);
+        textForReview = (EditText) findViewById(R.id.EditText01);
 
         //assign string to our textview
         textForReview.setText(text);
         
+        speakButton = 
+        	(Button) findViewById(R.id.button1);
+        
+        speakButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		sayit(textForReview.getText().toString());
+	    	}
+	    	});
+        mTts = new TextToSpeech(this,this); //init text to speech
     }
     
-    private void lauchActivity(int activity){
-    	
-    	Intent intent = null;
-		Bundle bundle = new Bundle();
-	
-		switch(activity){
-		
-			case 1:
-				//bundle.putString("DEFAULTTEXT", "Twitter");
-				intent = new Intent(this , mainMenu.class);
-				break;
-			case 2:
-				//bundle.putString("DEFAULTTEXT", "Facebook");
-				intent = new Intent(this , onAir.class);
-				break;
-//			case 3:
-//				bundle.putString("DEFAULTTEXT", "Google Buzz");
-//				intent = new Intent(this , onAir.class);
-//				break;
-				
-		}//end switch
-		
-		//intent.putExtras(bundle);
-		startActivity(intent);
-    	
+    private void sayit(String x) {
+		mTts.speak(x, TextToSpeech.QUEUE_FLUSH, null);
     }
+	
+    
+ //   @Override
+	public void onInit(int status) {
+		
+		if (status == TextToSpeech.SUCCESS) {
+			
+			int result = mTts.setLanguage(Locale.getDefault());
+			
+			if (result == TextToSpeech.LANG_MISSING_DATA
+					|| result == TextToSpeech.LANG_NOT_SUPPORTED) {
+				mTts.setLanguage(Locale.US);
+			} 
+		//	test=textForReview;
+			sayit(textForReview.getText().toString());
+			
+		}
+		
+	}
+	
+	@Override
+	public void onDestroy() {
+		// kill it!
+		if (mTts != null) {
+			mTts.stop();
+			mTts.shutdown();
+		}
+
+		super.onDestroy();
+	}
+	
 }
+	    		
+        
+       
+        
+       
+        
