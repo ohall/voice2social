ca01b55f429ac50437647103be9d4ab4e8aa74d0
apolkosnik <apolkosnik@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-11-27 07:37:08 +0000

    Implemented preference screens, that show up after pressing the menu button on the main screen. It can be extended further, eliminating media select.

--- trunk/Voice_2_Social/src/com/v2s/prefsActivity.java
+++ trunk/Voice_2_Social/src/com/v2s/prefsActivity.java
@@ -1,16 +1,15 @@
 package com.v2s;
 
+//import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-
-
 public class prefsActivity extends PreferenceActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.prefs);
-    }
-    
+		addPreferencesFromResource(R.xml.prefs);
+	}
+
 }
