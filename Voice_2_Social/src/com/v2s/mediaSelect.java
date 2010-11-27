081f9160d4641d1521099a2278193e8e25475120
apolkosnik <apolkosnik@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-11-27 06:20:38 +0000

    Added exception handling to startActivityForResult(), so that it doesn't crash when voice recognizer is not present!

--- Voice_2_Social/src/com/v2s/mediaSelect.java
+++ Voice_2_Social/src/com/v2s/mediaSelect.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -12,163 +13,162 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class mediaSelect extends Activity implements TextToSpeech.OnInitListener {
-    /** Called when the activity is first created. */
-	
+public class mediaSelect extends Activity implements
+		TextToSpeech.OnInitListener {
+	/** Called when the activity is first created. */
+
 	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
 	private static final String INSTRUCTIONS = "Please say: Twitter, Facebook or Buzz";
-    private TextToSpeech mTts; 
+	private TextToSpeech mTts;
 
-	
-	//flag lets us know if we've disabled voice recog in mainMenu
-    private Boolean _speechIsDisabled = false;
-	
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mediaselect);
-        
-        //get bundle from intent and assign it to a string;
-        Bundle incomingBundle = this.getIntent().getExtras();
-     //   _speechIsDisabled = incomingBundle.getBoolean("VR_DISABLED",false);
-        
-        
-        Button twitterButton = 
-        	(Button) findViewById(R.id.twitterButton);
-        
-        Button fbButton = 
-        	(Button) findViewById(R.id.fbButton);
-        
-        Button buzzButton = 
-        	(Button) findViewById(R.id.buzzButton);
-        
-        twitterButton.setOnClickListener(new OnClickListener() {
-        	public void onClick(View view){
-        		//start review and send activity
-        		launchActivity(1);
-        	}
-        });
-    	
-        fbButton.setOnClickListener(new OnClickListener() {
-        	public void onClick(View view){
-        		//start review and send activity
-        		launchActivity(2);
-        	}
-        });
-        
-        buzzButton.setOnClickListener(new OnClickListener() {
-        	public void onClick(View view){
-        		//start review and send activity
-        		launchActivity(3);
-        	}
-        });
-        
-        /**
-         * Text to Speech for Instructions
-         */
-        mTts = new TextToSpeech(this,this); //init text to speech
-        
-    }
-    
-    private void startVoiceRecognitionActivity() {
-		
-    	Intent commandIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        commandIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
-        //commandIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
-        startActivityForResult(commandIntent, VOICE_RECOGNITION_REQUEST_CODE);
-    }
-    
-    
-    private void launchActivity(int activity) {
-    	
+	// flag lets us know if we've disabled voice recog in mainMenu
+	private Boolean _speechIsDisabled = false;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mediaselect);
+
+		// get bundle from intent and assign it to a string;
+		Bundle incomingBundle = this.getIntent().getExtras();
+		// _speechIsDisabled = incomingBundle.getBoolean("VR_DISABLED",false);
+
+		Button twitterButton = (Button) findViewById(R.id.twitterButton);
+
+		Button fbButton = (Button) findViewById(R.id.fbButton);
+
+		Button buzzButton = (Button) findViewById(R.id.buzzButton);
+
+		twitterButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				// start review and send activity
+				launchActivity(1);
+			}
+		});
+
+		fbButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				// start review and send activity
+				launchActivity(2);
+			}
+		});
+
+		buzzButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				// start review and send activity
+				launchActivity(3);
+			}
+		});
+
+		/**
+		 * Text to Speech for Instructions
+		 */
+		mTts = new TextToSpeech(this, this); // init text to speech
+
+	}
+
+	private void startVoiceRecognitionActivity() {
+
+		Intent commandIntent = new Intent(
+				RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		commandIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+				RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+		// commandIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+		// "Speech recognition demo");
+        try {
+        	startActivityForResult(commandIntent, VOICE_RECOGNITION_REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+        	// say the exception!!! :-)
+        	sayit("Voice recognizer not present!");
+        }
+	}
+
+	private void launchActivity(int activity) {
+
 		Intent intent = null;
 		Bundle bundle = new Bundle();
-	
-		switch(activity){
-		
-			case 1:
-				bundle.putString("DEFAULTTEXT", "Twitter");
-				intent = new Intent(this , onAir.class);
-				break;
-			case 2:
-				bundle.putString("DEFAULTTEXT", "Facebook");
-				intent = new Intent(this , onAir.class);
-				break;
-			case 3:
-				bundle.putString("DEFAULTTEXT", "Google Buzz");
-				intent = new Intent(this , onAir.class);
-				break;
-				
-		}//end switch
-		
+
+		switch (activity) {
+
+		case 1:
+			bundle.putString("DEFAULTTEXT", "Twitter");
+			intent = new Intent(this, onAir.class);
+			break;
+		case 2:
+			bundle.putString("DEFAULTTEXT", "Facebook");
+			intent = new Intent(this, onAir.class);
+			break;
+		case 3:
+			bundle.putString("DEFAULTTEXT", "Google Buzz");
+			intent = new Intent(this, onAir.class);
+			break;
+
+		}// end switch
+
 		intent.putExtras(bundle);
 		startActivity(intent);
-	
-	}//end launchActivity
 
-    
-    private void sayit(String x) {
+	}// end launchActivity
+
+	private void sayit(String x) {
 		mTts.speak(x, TextToSpeech.QUEUE_FLUSH, null);
 	}
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        
-    	int selection = 0;
 
-    	if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
-        	
-        	ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-        	String selectCommand = ""; 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        	
-        	selectCommand = matches.get(0);
-        	
-        	if( selectCommand.compareToIgnoreCase("twitter") == 0 ){
-        		selection = 1;
-        	}else if(selectCommand.compareToIgnoreCase("facebook") == 0){
-        		selection = 2;
-        	}else if(selectCommand.compareToIgnoreCase("buzz") == 0){
-        		selection = 3;
-        	}
+		int selection = 0;
 
-        	 super.onActivityResult(requestCode, resultCode, data);
-             
-        	 if(selection != 0){
-        		 launchActivity(selection);
-        	 }else{
-        		 startVoiceRecognitionActivity();
-        	 }
-        }
+		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE
+				&& resultCode == RESULT_OK) {
 
-       
-    }
-    
+			ArrayList<String> matches = data
+					.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+			String selectCommand = "";
+
+			selectCommand = matches.get(0);
+
+			if (selectCommand.compareToIgnoreCase("twitter") == 0) {
+				selection = 1;
+			} else if (selectCommand.compareToIgnoreCase("facebook") == 0) {
+				selection = 2;
+			} else if (selectCommand.compareToIgnoreCase("buzz") == 0) {
+				selection = 3;
+			}
+
+			super.onActivityResult(requestCode, resultCode, data);
+
+			if (selection != 0) {
+				launchActivity(selection);
+			} else {
+				startVoiceRecognitionActivity();
+			}
+		}
+
+	}
 
 	@Override
 	public void onInit(int status) {
-		
-		if(!_speechIsDisabled){
-			
+
+		if (!_speechIsDisabled) {
+
 			if (status == TextToSpeech.SUCCESS) {
-				
+
 				int result = mTts.setLanguage(Locale.getDefault());
-				
+
 				if (result == TextToSpeech.LANG_MISSING_DATA
 						|| result == TextToSpeech.LANG_NOT_SUPPORTED) {
 					mTts.setLanguage(Locale.US);
-				} 
-				
+				}
+
 				sayit(INSTRUCTIONS);
-				
+
 				startVoiceRecognitionActivity();
 			}
-        }
-		
+		}
+
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		// kill it!
@@ -179,8 +179,5 @@ public class mediaSelect extends Activity implements TextToSpeech.OnInitListener
 
 		super.onDestroy();
 	}
-    
-    
-    
-    
+
 }
