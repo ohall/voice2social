973af2f3b29b69ff9b5791f7e27b16ff74e18616
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-11-30 22:45:46 +0000

    added jtwitter,  very beta

--- Voice_2_Social/src/com/v2s/reviewAndSend.java
+++ Voice_2_Social/src/com/v2s/reviewAndSend.java
@@ -3,18 +3,26 @@ package com.v2s;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+import winterwell.jtwitter.Twitter;
+
 public class reviewAndSend extends Activity implements TextToSpeech.OnInitListener {
 	
 	private EditText textForReview;
     private Button speakButton;
+    private Button recordAgainButton;
     private TextToSpeech mTts; 
+    Twitter twitter;
+    SharedPreferences prefs;
     
     
     /** Called when the activity is first created. */
@@ -31,15 +39,43 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
         //assign string to our textview
         textForReview.setText(text);
         
+        //Initialize twitter
+        //prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //String username = prefs.getString("username", "n/a");
+        //String password = prefs.getString("password", "n/a");
+        //if (username != null && password != null){
+          twitter = new Twitter("v2s", "voice2social");
+          twitter.setAPIRootUrl("http://identi.ca/api");
+        //}
+        
         speakButton = 
         	(Button) findViewById(R.id.button1);
         
         speakButton.setOnClickListener(new OnClickListener() {
 	    	public void onClick(View view){
 	    		sayit(textForReview.getText().toString());
+	    		
+	    			String status = textForReview.getText().toString();
+	           // set twitter status 
+	              twitter.setStatus(status);
 	    	}
 	    	});
         mTts = new TextToSpeech(this,this); //init text to speech
+        
+        recordAgainButton = 
+        	(Button) findViewById(R.id.button2);
+        
+        recordAgainButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+	    		
+	    		//launch record for twitter.
+	    		launchActivity(1);
+	    		
+	    	}
+	    	});
+        
+        
+
     }
     
     private void sayit(String x) {
@@ -64,6 +100,31 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
 		}
 		
 	}
+	
+	
+	private void launchActivity(int activity) {
+
+		Intent intent = null;
+		Bundle bundle = new Bundle();
+
+		switch (activity) {
+
+		case 1:
+			bundle.putString("DEFAULTTEXT", "Twitter");
+			intent = new Intent(this, onAir.class);
+			break;
+		case 2:
+			bundle.putString("DEFAULTTEXT", "Facebook");
+			intent = new Intent(this, onAir.class);
+			break;
+		case 3:
+			bundle.putString("DEFAULTTEXT", "Google Buzz");
+			intent = new Intent(this, onAir.class);
+			break;
+
+		}// end switch
+	}
+	
 	
 	@Override
 	public void onDestroy() {
