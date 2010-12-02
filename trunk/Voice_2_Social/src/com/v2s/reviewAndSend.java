e33f57dcde690d91ee9b4502a25ac440f522024c
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-12-02 06:28:21 +0000

    Implemented twitter posting functionality to less secure Identi.ca.  Added some exception handling.  Included stubs and error messages from remaining networks.  Connected activity to prefs data.

--- trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
+++ trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
@@ -2,25 +2,31 @@ package com.v2s;
 
 import java.util.Locale;
 
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-
-import winterwell.jtwitter.Twitter;
+import android.widget.Toast;
 
 public class reviewAndSend extends Activity implements TextToSpeech.OnInitListener {
 	
 	private EditText textForReview;
     private Button speakButton;
     private Button recordAgainButton;
+    private Button postRecordingButton;
     private TextToSpeech mTts; 
+    private String networkSelected;
     Twitter twitter;
     SharedPreferences prefs;
     
@@ -30,47 +36,98 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviewandsend);
-        //get bundle from intent and assign it to a string;
-        Bundle incomingBundle = this.getIntent().getExtras();
-        String text = incomingBundle.getString("DEFAULTTEXT");
         
-        textForReview = (EditText) findViewById(R.id.EditText01);
 
-        //assign string to our textview
+    	Bundle incomingBundle = this.getIntent().getExtras();
+    	final String text = incomingBundle.getString("DEFAULTTEXT");
+    	networkSelected = incomingBundle.getString("NETWORKSELECTED");        
+    
+        textForReview = (EditText) findViewById(R.id.EditText01);
         textForReview.setText(text);
         
-        //Initialize twitter
-        //prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //String username = prefs.getString("username", "n/a");
-        //String password = prefs.getString("password", "n/a");
-        //if (username != null && password != null){
-          twitter = new Twitter("v2s", "voice2social");
-          twitter.setAPIRootUrl("http://identi.ca/api");
-        //}
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
         
-        speakButton = 
-        	(Button) findViewById(R.id.button1);
+       
+        //Deal with selected social network
         
+        if(networkSelected == "Twitter"){
+        //Get login data for twitter account from prefs and log into twitter.
+	        String username = prefs.getString("t_user", "n/a");
+	        String password = prefs.getString("t_password", "n/a");
+	        if (username != null && password != null){
+	          twitter = new Twitter(username, password);
+	          twitter.setAPIRootUrl("http://identi.ca/api");
+	        }else{
+	        	Builder builder = new AlertDialog.Builder(this);
+	        	builder.setTitle("Set Twitter Login Info");
+	        	builder.setMessage("Please set your Twitter username and password using the menu button");
+	        	builder.setPositiveButton("OK",null);
+	        }
+        }else if(networkSelected == "Facebook"){
+        	
+        	//TODO: Implement this network, remove toast and activity launch
+        	Toast.makeText(reviewAndSend.this, 
+	        		  "Facebook not yet configured", 
+	        		  Toast.LENGTH_SHORT).show();
+        	launchActivity(5);
+        	
+        }else if(networkSelected == "Buzz"){
+        	
+        	//TODO: Implement this network, remove toast and activity launch
+        	Toast.makeText(reviewAndSend.this, 
+	        		  "Buzz not yet configured", 
+	        		  Toast.LENGTH_SHORT).show();
+        	launchActivity(5);
+        	
+        }
+
+        
+        speakButton = (Button) findViewById(R.id.button1);
         speakButton.setOnClickListener(new OnClickListener() {
 	    	public void onClick(View view){
 	    		sayit(textForReview.getText().toString());
-	    		
-	    			String status = textForReview.getText().toString();
-	           // set twitter status 
-	              twitter.setStatus(status);
 	    	}
 	    	});
+        
         mTts = new TextToSpeech(this,this); //init text to speech
         
-        recordAgainButton = 
-        	(Button) findViewById(R.id.button2);
-        
+        recordAgainButton = (Button) findViewById(R.id.button2);
         recordAgainButton.setOnClickListener(new OnClickListener() {
 	    	public void onClick(View view){
-	    		
 	    		//launch record for twitter.
-	    		launchActivity(1);
-	    		
+	            if(networkSelected == "Twitter"){
+	            	launchActivity(1);
+	             }else if(networkSelected == "Facebook"){
+		            launchActivity(2);
+	      	
+	              }else if(networkSelected == "Buzz"){
+		            launchActivity(3);
+	                	
+	              }
+	    	}
+	    	});
+        
+        
+        postRecordingButton = (Button) findViewById(R.id.button3);
+        postRecordingButton.setText("Post Recording to "+networkSelected);
+        postRecordingButton.setOnClickListener(new OnClickListener() {
+	    	public void onClick(View view){
+		           // set twitter status, catch exceptions. 
+	    		try{
+	              twitter.setStatus(text);
+	              Toast.makeText(reviewAndSend.this, 
+	            		  "Twitter Post Success!", Toast.LENGTH_SHORT).show();
+	    		}catch(TwitterException.E401 e){
+		          Toast.makeText(reviewAndSend.this, 
+		        		  "Wrong Username or Password. Please check logins.", 
+		        		  Toast.LENGTH_SHORT).show();
+
+	    		}catch(Exception e){
+	    			Toast.makeText(reviewAndSend.this, 
+	    					"Network Host not responding",Toast.LENGTH_SHORT).show();
+	    		}
+	    		//return to main menu
+	    		launchActivity(4);
 	    	}
 	    	});
         
@@ -118,11 +175,20 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
 			intent = new Intent(this, onAir.class);
 			break;
 		case 3:
-			bundle.putString("DEFAULTTEXT", "Google Buzz");
+			bundle.putString("DEFAULTTEXT", "Buzz");
 			intent = new Intent(this, onAir.class);
+			break;
+		case 4:
+			intent = new Intent(this, mainMenu.class);
+			break;
+		case 5:
+			intent = new Intent(this, mediaSelect.class);
 			break;
 
 		}// end switch
+		
+		intent.putExtras(bundle);
+		startActivity(intent);
 	}
 	
 	
