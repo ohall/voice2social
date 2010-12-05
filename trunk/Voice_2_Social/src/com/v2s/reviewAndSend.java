68d6659aabb07852b81692cac4e7b36f16910d72
oakley349@gmail.com <oakley349@gmail.com@a53ede82-f259-bf9f-dc04-c9b2c85b2682>
2010-12-05 04:02:12 +0000

    cleaned up code. added constants. added logic to deal with social network selection.  added some popups to in cases where username and password are incorrect or not provided. Stubs for adding facebook and buzz interfaces.

--- trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
+++ trunk/Voice_2_Social/src/com/v2s/reviewAndSend.java
@@ -12,7 +12,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -27,61 +26,34 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
     private Button postRecordingButton;
     private TextToSpeech mTts; 
     private String networkSelected;
-    Twitter twitter;
-    SharedPreferences prefs;
+    private String username;
+    private String password;
+    private Twitter twitter;
+    private SharedPreferences prefs;
+    
+    private static final	int GOTO_TWITTER		= 1;
+	private static final 	int GOTO_FACEBOOK 		= 2;
+	private static final 	int GOTO_BUZZ 			= 3;
+	private static final 	int GOTO_MAIN_MENU 		= 4;
+	private static final 	int GOTO_MEDIA_SELECT 	= 5;
     
     
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.reviewandsend);
         
-
-    	Bundle incomingBundle = this.getIntent().getExtras();
-    	final String text = incomingBundle.getString("DEFAULTTEXT");
-    	networkSelected = incomingBundle.getString("NETWORKSELECTED");        
-    
-        textForReview = (EditText) findViewById(R.id.EditText01);
+    	Bundle incomingBundle 	= this.getIntent().getExtras();
+    	final String text 		= incomingBundle.getString("DEFAULTTEXT");
+    	
+    	networkSelected 		= incomingBundle.getString("NETWORKSELECTED");        
+        textForReview 			= (EditText) findViewById(R.id.EditText01);
         textForReview.setText(text);
+        prefs 					= PreferenceManager.getDefaultSharedPreferences(this);
         
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        
-       
-        //Deal with selected social network
-        
-        if(networkSelected == "Twitter"){
-        //Get login data for twitter account from prefs and log into twitter.
-	        String username = prefs.getString("t_user", "n/a");
-	        String password = prefs.getString("t_password", "n/a");
-	        if (username != null && password != null){
-	          twitter = new Twitter(username, password);
-	          twitter.setAPIRootUrl("http://identi.ca/api");
-	        }else{
-	        	Builder builder = new AlertDialog.Builder(this);
-	        	builder.setTitle("Set Twitter Login Info");
-	        	builder.setMessage("Please set your Twitter username and password using the menu button");
-	        	builder.setPositiveButton("OK",null);
-	        }
-        }else if(networkSelected == "Facebook"){
-        	
-        	//TODO: Implement this network, remove toast and activity launch
-        	Toast.makeText(reviewAndSend.this, 
-	        		  "Facebook not yet configured", 
-	        		  Toast.LENGTH_SHORT).show();
-        	launchActivity(5);
-        	
-        }else if(networkSelected == "Buzz"){
-        	
-        	//TODO: Implement this network, remove toast and activity launch
-        	Toast.makeText(reviewAndSend.this, 
-	        		  "Buzz not yet configured", 
-	        		  Toast.LENGTH_SHORT).show();
-        	launchActivity(5);
-        	
-        }
-
-        
+              
         speakButton = (Button) findViewById(R.id.button1);
         speakButton.setOnClickListener(new OnClickListener() {
 	    	public void onClick(View view){
@@ -96,13 +68,12 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
 	    	public void onClick(View view){
 	    		//launch record for twitter.
 	            if(networkSelected == "Twitter"){
-	            	launchActivity(1);
+	            	launchActivity(GOTO_TWITTER);
 	             }else if(networkSelected == "Facebook"){
-		            launchActivity(2);
+		            launchActivity(GOTO_FACEBOOK);
 	      	
 	              }else if(networkSelected == "Buzz"){
-		            launchActivity(3);
-	                	
+		            launchActivity(GOTO_BUZZ);
 	              }
 	    	}
 	    	});
@@ -123,11 +94,13 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
 		        		  Toast.LENGTH_SHORT).show();
 
 	    		}catch(Exception e){
-	    			Toast.makeText(reviewAndSend.this, 
-	    					"Network Host not responding",Toast.LENGTH_SHORT).show();
+	    			
+	    				Toast.makeText(reviewAndSend.this, 
+	    					"Network Host not responding. Check User Name and Password in Preferences Menu",
+	    					Toast.LENGTH_LONG).show();
 	    		}
 	    		//return to main menu
-	    		launchActivity(4);
+	    		launchActivity(GOTO_MAIN_MENU);
 	    	}
 	    	});
         
@@ -144,18 +117,46 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
 	public void onInit(int status) {
 		
 		if (status == TextToSpeech.SUCCESS) {
-			
 			int result = mTts.setLanguage(Locale.getDefault());
-			
 			if (result == TextToSpeech.LANG_MISSING_DATA
 					|| result == TextToSpeech.LANG_NOT_SUPPORTED) {
 				mTts.setLanguage(Locale.US);
-			} 
-		//	test=textForReview;
-			sayit(textForReview.getText().toString());
-			
+			}
+			sayit(textForReview.getText().toString());	
 		}
 		
+        //Deal with selected social network
+        if(networkSelected.compareTo("Twitter") == 0){
+        //Get login data for twitter account from prefs and log into twitter.
+	        username = prefs.getString("t_user", null);
+	        password = prefs.getString("t_password", null);
+	        if (username != null && password != null){
+	          twitter = new Twitter(username, password);
+	          //Set Re-rout to identi while resolving twitter issues.
+	          twitter.setAPIRootUrl("http://identi.ca/api");
+	        }else{
+	        	Builder builder = new AlertDialog.Builder(this);
+	        	builder.setTitle("Set Twitter Login Info");
+	        	builder.setMessage("Please set your Twitter username and password using the menu button");
+	        	builder.setPositiveButton("OK",null);
+	        	builder.show();
+	        }
+        }else if(networkSelected.compareTo("Facebook") == 0){
+        	
+        	//TODO: Implement this network, remove toast and activity launch
+        	Toast.makeText(reviewAndSend.this, 
+	        		  "Facebook not yet configured", 
+	        		  Toast.LENGTH_LONG).show();
+        	launchActivity(GOTO_MEDIA_SELECT);
+        	
+        }else if( networkSelected.compareTo("Buzz") == 0){
+        	
+        	//TODO: Implement this network, remove toast and activity launch
+        	Toast.makeText(reviewAndSend.this, 
+	        		  "Buzz not yet configured", 
+	        		  Toast.LENGTH_LONG).show();
+        	launchActivity(GOTO_MEDIA_SELECT);	
+        }
 	}
 	
 	
@@ -166,22 +167,22 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
 
 		switch (activity) {
 
-		case 1:
+		case GOTO_TWITTER:
 			bundle.putString("DEFAULTTEXT", "Twitter");
 			intent = new Intent(this, onAir.class);
 			break;
-		case 2:
+		case GOTO_FACEBOOK:
 			bundle.putString("DEFAULTTEXT", "Facebook");
 			intent = new Intent(this, onAir.class);
 			break;
-		case 3:
+		case GOTO_BUZZ:
 			bundle.putString("DEFAULTTEXT", "Buzz");
 			intent = new Intent(this, onAir.class);
 			break;
-		case 4:
+		case GOTO_MAIN_MENU:
 			intent = new Intent(this, mainMenu.class);
 			break;
-		case 5:
+		case GOTO_MEDIA_SELECT:
 			intent = new Intent(this, mediaSelect.class);
 			break;
 
@@ -203,10 +204,4 @@ public class reviewAndSend extends Activity implements TextToSpeech.OnInitListen
 		super.onDestroy();
 	}
 	
-}
-	    		
-        
-       
-        
-       
-        
+}   
